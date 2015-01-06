8624936c4fa1619e6c761c089ccead0e72bb7b88
silverXz <wanghaoyu00x@gmail.com>
2015-01-06 09:33:12 +0800

    Update Set Matrix Zeroes.cpp

--- Set Matrix Zeroes.cpp
+++ Set Matrix Zeroes.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
 
     // Solution one: Use O(M+N) Space. There're still room for improvement!
-    void setZeroes(vector<vector<int> > &matrix) {
-        
+    void setZeroes(vector<vector<int> > &matrix) 
+    {
         set<int> row;
     	set<int> colume;
     	for( int i = 0 ; i < matrix.size() ; i++ )
@@ -27,4 +27,33 @@ public:
 			    matrix[i][*itr] = 0;
     	}
     }
+    
+    // Solution2: O(1) space occupation
+     void setZeroes2(vector<vector<int> > &matrix) 
+     {
+     	int col = 1;
+	for( int i = 0 ; i < matrix.size() ; i++ )
+	{
+		if( matrix[i][0] == 0 )
+			col = 0;
+		for( int j = 1 ; j < matrix[i].size() ; j++ )
+			if( matrix[i][j] == 0 )
+			{
+				matrix[i][0] = 0;
+				matrix[0][j] = 0;
+			}
+	}
+	
+	for( int i = matrix.size() - 1 ; i >= 0 ; i-- )
+	{
+		for( int j = matrix[i].size() - 1 ; j >= 1 ; j-- )
+		{
+			if( matrix[i][0] == 0 || matrix[0][j] == 0 )
+				matrix[i][j] = 0;
+		}
+		if( col == 0 )
+			matrix[i][0] = 0;
+	}
+     }
+    
 };
