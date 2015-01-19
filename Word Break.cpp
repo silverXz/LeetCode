b4e4bc710ceb4cb7810de08763fd0923ac0ae903
silverXz <wanghaoyu00x@gmail.com>
2015-01-19 13:15:30 +0800

    Update Word Break.cpp

--- Word Break.cpp
+++ Word Break.cpp
@@ -23,3 +23,30 @@ public:
         return res;
     }
 };
+
+// Solution #2 : Accepted!!!!!! With Dynamical Programming!
+
+class Solution {
+public:
+    bool wordBreak(string s, unordered_set<string> &dict) {
+        if( dict.size() == 0 )
+            return false;
+        vector<bool> dp(s.length()+1,false);
+        dp[0] = true;
+        for( int i = 1 ; i <= s.length() ; i++ )
+        {
+            for( int j = i-1; j >= 0 ; j-- )
+            {
+                if( dp[j] )
+                {
+                    if( dict.find(s.substr(j,i-j)) != dict.end() )
+                    {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+};
