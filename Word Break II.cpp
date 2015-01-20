e07e09ecace8e5281926d25b6b2c8415609ffe72
silverXz <wanghaoyu00x@gmail.com>
2015-01-20 15:39:32 +0800

    Create Word Break II.cpp

---
+++ Word Break II.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+
+    vector<string> pathTrace(vector<vector<int>>& path, const string& s, int id)
+    {
+        vector<string> result;
+        if( id <= 0 )
+        {
+            result.push_back("");
+            return result;
+        }
+        for( int i = 0 ; i < path[id].size() ; i++ )
+        {
+            string str = s.substr(path[id][i],id - path[id][i]);
+            vector<string> subStr = pathTrace(path,s,path[id][i]);
+            for( int j = 0 ; j < subStr.size() ; j++ )
+            {
+                if( subStr[j].size() == 0 )
+                    result.push_back(str);
+                else
+                    result.push_back(subStr[j] + " " + str);
+            }
+        }
+        return result;
+    }
+
+    vector<string> wordBreak(string s, unordered_set<string> &dict) 
+    {
+        vector<bool> dp(s.length()+1,false);
+        dp[0] = true;
+        vector<vector<int>> path(s.length() + 1);
+        path[0].push_back(0);
+        for( int i = 1; i <= s.length() ; i++ )
+        {
+            for( int j = i-1 ; j >= 0 ; j-- )
+            {
+                if( dp[j] && dict.find(s.substr(j,i-j)) != dict.end() )
+                {
+                    dp[i] = true;
+                    path[i].push_back(j);
+                }
+            }
+        }
+        return pathTrace(path,s,s.length());
+    }
+};
